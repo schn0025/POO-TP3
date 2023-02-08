6454aa6cc212b6bc5d714f02342c059828089ea3
Arthur Schneider <arthur.schneider@etudiant.univ-reims.fr>
2023-02-08 16:55:35 +0100

    ajour de l'exercice 2 jusqu'a la question 5 fini

---
+++ TestWeighingTicket.php
@@ -0,0 +1,25 @@
+<?php
+// Schneider Arthur
+declare(strict_types = 1);
+require_once "WeighingTicket.php";
+
+// question 3
+$inconnu = new WeighingTicket;
+$carotte = new WeighingTicket("Carotte");
+$concombre = new WeighingTicket("Concombre",0.99);
+$pommeDT = new WeighingTicket("Pomme de Terre",1.5,3000);
+
+// question 4
+echo "\nquestion 4\n";
+echo $inconnu->getArticleName(), "\n";
+echo $carotte->getWeight(), "\n";
+echo $concombre->getPricePerKilogram(), "\n";
+echo $pommeDT->getPrice(), "\n";
+echo $pommeDT->getWeight(), "\n";
+
+// question 5
+echo "\nquestion 5 \n";
+echo $inconnu->setArticleName('kiwi');
+echo $inconnu->setPricePerKilogram(3.5);
+echo $inconnu->setWeight(1050);
+echo $inconnu->getPrice(),"\n";
---
+++ WeighingTicket.php
@@ -0,0 +1,99 @@
+<?php
+// Schneider Arthur
+declare(strict_types = 1);
+
+// question 2
+class WeighingTicket{
+    private string $articleName;
+    private float $pricePerKilogram;
+    private int $weight;
+    private float $price;
+    /**
+     * constructeur de la classe WeighingTicket, permet d'afecte un nom d'article, un prix
+     * au kilo ainsi qu'un poids a un tiket. Lorsque ces caractéritiques ne sont pas renseignées lors de
+     * l’appel du contructeur, le tiket auras pour nom d'article "Unknown", pour prix au kilo 0
+     * et pour poids 0.
+     * 
+     * @param string $articleName (optional) nom de l'article
+     * @param float $pricePerKilogram (optional) prix au kilo de l'article
+     * @param int $weight (optional) poids de l'article
+     */
+    public function __construct(string $articleName = "Unknown", float $pricePerKilogram = 0, int $weight = 0)
+    {
+        $this->articleName = $articleName;
+        $this->pricePerKilogram = $pricePerKilogram;
+        $this->weight = $weight;
+        $this->price = 0;
+        if($this->weight != 0){
+            $this->price = $pricePerKilogram / 1000 * $weight;
+        }
+    }
+// question 4
+    /**
+     * accesseur au nom de l'article, renvoi le nom de l'article avec une chaine de caractaire
+     * @return string nom de l'article
+     */
+    public function getArticleName(): string
+    {
+        return $this->articleName;
+    }
+    /**
+     * accesseur au Prix au kilo de l'article, renvoi le prix au kilo de l'aricle avec un réel
+     * @return float prix au kilo de l'article
+     */
+    public function getPricePerKilogram():float
+    {
+        return $this->pricePerKilogram;
+    }
+    /**
+     * accesser au poids de l'article, renvoi le poids de l'article avec un entier
+     * @return int
+     */
+    public function getWeight():int
+    {
+        return $this->weight;
+    }
+    /**
+     * accesseur au prix de l'article, renvois le pris de l'article avec un réel
+     * @return float pris de l'aricle
+     */
+    public function getPrice():float
+    {
+        return $this->price;
+    }
+    /**
+     * modificateurs du nom de l'article, modifie l'article avec le nom passer en parametre
+     * @param mixed $articleName nouveux nom
+     */
+    public function setArticleName($articleName): void
+    {
+        $this->articleName = $articleName;
+    }
+    /**
+     * modificateur du prix au kilo du produit, modifie le prix au kilo du produit
+     * et remet a jour de prix
+     * @param mixed $pricePerKilogram nouveau
+     */
+    public function setPricePerKilogram($pricePerKilogram): void
+    {
+        $this->pricePerKilogram = $pricePerKilogram;
+        $this->updatePrice();
+    }
+    /**
+     * modificateur du poids du produit, modifie le poids du produit et met à jour son prix
+     * @param mixed $weight nouveau poids
+     */
+    public function setWeight($weight): void
+    {
+        $this->weight = $weight;
+        $this->updatePrice();
+    }
+    /**
+     * methode qui met a jour le prix du produit
+     * @return void
+     */
+    private function updatePrice(): void
+    {
+        $this->price = $this->pricePerKilogram / 1000 * $this->weight;
+    }
+}
